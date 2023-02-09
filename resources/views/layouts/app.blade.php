f943f6677ae6fdc8ec917362c8c6d11b77a4fb61
LorenzoOgnibene <lorenzo.ognibene@gmail.com>
2023-02-09 16:45:12 +0100

    added cards form db into view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,22 @@
         @vite('resources/js/app.js')
     </head>
     <body>
-
+        <div class="container">
+            <div class="row row-cols-3 g-4">
+                @foreach ($movies as $movie)
+                    <div class="col">
+                        <div class="card" style="">
+                            <div class="card-body">
+                              <h5 class="card-title">{{$movie->title}}</h5>
+                              <h6 class="card-subtitle mb-2 text-muted">{{$movie->original_title}}</h6>
+                              <p class="card-text">{{$movie->nationality}}</p>
+                              <p class="card-text">{{$movie->date}}</p>
+                              <p class="card-text">{{$movie->vote}}</p>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
     </body>
 </html>
